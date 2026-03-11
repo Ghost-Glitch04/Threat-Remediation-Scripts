a4a84660290e904fda1830d383d39bf220483120
Ghost-Glitch04 <176685137+Ghost-Glitch04@users.noreply.github.com>
2026-03-11 12:09:34 -0400

    Script "Remove-McAfeeWebAdvisor.ps1" will run unattented in N-Able. Removing "Write-Host" output.

--- Mcafee/Remove-McAfeeWebAvisor.ps1
+++ Mcafee/Remove-McAfeeWebAvisor.ps1
@@ -6,7 +6,6 @@ $webAdvisorPath = "C:\Program Files\McAfee\WebAdvisor"
 
 # Test for Presence of McAfee WebAdvisor
 if ( -not (Test-Path -Path $webAdvisorPath) ) {
-    Write-Output "McAfee WebAdvisor is not installed on this system."
     exit 0
 }
 
@@ -16,15 +15,12 @@ $uninstallerName = "uninstaller.exe"
 # Test for Presence of Uninstaller
 $uninstallerPath = Join-Path -Path $webAdvisorPath -ChildPath $uninstallerName
 if ( -not (Test-Path -Path $uninstallerPath) ) {
-    Write-Output "McAfee WebAdvisor uninstaller is not found."
     exit 0
 }
 
 # Execute the Uninstaller
 try {
     Start-Process -FilePath $uninstallerPath -ArgumentList "/s" -Wait -NoNewWindow
-    Write-Output "McAfee WebAdvisor uninstallation initiated."
 } catch {
-    Write-Output "Failed to execute the McAfee WebAdvisor uninstaller: $_"
     exit 1
 }
